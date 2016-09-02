db83809e677888ba877114eb5366fa83f981439c
Lars Kluge <l@larskluge.com>
2016-09-02 18:08:12 +0200

    Write blob chunk when data arrives, not only when complete file arrived

--- upload-server.go
+++ upload-server.go
@@ -33,21 +33,24 @@ func (s *server) Info(ctx context.Context, in *pb.Empty) (*pb.InfoResponse, erro
 }
 
 func (s *server) Upload(stream pb.Storage_UploadServer) error {
+	var wg sync.WaitGroup
 	id := GenBlobId()
 	key := blobKey(id)
 	if cache.Exists(key) {
 		log.WithFields(log.Fields{"blob_key": key}).Error("Newly generated blob key is already in use, this should not happen")
 		return errors.New(fmt.Sprintf("Newly generated blob key '%s' is already in use, this should not happen", key))
 	}
-
 	log.WithFields(log.Fields{"blob_id": id}).Info("Incoming upload")
-
-	var wg sync.WaitGroup
 
 	wg.Add(1)
 	go func() {
-		blob := []byte{}
+		_, blob, err := cache.Get(blobKey(id))
+		check(err)
+		if blob == nil {
+			panic("did not get a writer..?")
+		}
 		chunks := 0
+		bytesWritten := 0
 		for {
 			r, err := stream.Recv()
 			if err == io.EOF {
@@ -64,12 +67,15 @@ func (s *server) Upload(stream pb.Storage_UploadServer) error {
 			} else {
 				check(err)
 				log.WithFields(log.Fields{"chunk_size": len(r.Chunk), "chunks": chunks}).Debug("Chunk received")
-				blob = append(blob, r.Chunk...)
-				writeBlob(id, r.Chunk)
+				n, err := blob.Write(r.Chunk)
+				check(err)
+				// blob.Flush()
+				bytesWritten += n
 				chunks += 1
 			}
 		}
-		log.WithFields(log.Fields{"blob_size": len(blob), "chunks": chunks}).Info("Upload completed")
+		log.WithFields(log.Fields{"blob_size": bytesWritten, "chunks": chunks}).Info("Upload completed")
+		blob.Close()
 		wg.Done()
 	}()
 
@@ -89,17 +95,5 @@ func (s *server) Upload(stream pb.Storage_UploadServer) error {
 
 	wg.Wait()
 
-	return nil
-}
-
-func writeBlob(id uint64, blob []byte) error {
-	r, w, err := cache.Get(blobKey(id))
-	check(err)
-	if w == nil {
-		panic("did not get a writer..?")
-	}
-	w.Write(blob)
-	w.Close()
-	r.Close()
 	return nil
 }
