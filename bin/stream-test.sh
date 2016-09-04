5e23117f4a00d083f7b780bb8a8c3c7c58e5f9cc
Lars Kluge <l@larskluge.com>
2016-09-04 23:00:11 +0200

    Add simple stream test script; pipe output into client

---
+++ bin/stream-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo hi, sleeping for 1s..
+sleep 1
+echo hello again, sleeping for 1s..
+sleep 1
+echo hello again, sleeping for 1s..
+sleep 1
+echo hello again, sleeping for 1s..
+sleep 1
+echo hello again, sleeping for 1s..
+sleep 1
+echo hello again, sleeping for 1s..
+sleep 1
+echo done
